17e4da9105b9a423acdea5f46f4320578fbe48b7
kenken0702 <kenta0702ito@gmail.com>
2026-07-30 05:11:06 +0000

    commit

--- maindva.py
+++ maindva.py
@@ -512,7 +512,7 @@ def run_practical_dva_experiment(
 
 if __name__ == "__main__":
     run_practical_dva_experiment(
-        tickers=["8088.T"],
+        tickers=["3080.T"],
         start_date="2016-01-01",
         end_date="2025-12-31",
         total_budget_us=60000,
